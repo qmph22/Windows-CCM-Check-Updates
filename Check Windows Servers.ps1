d74255a35a9aa97c85ce494ad7fdfa2279105b27
qmph22 <66791972+qmph22@users.noreply.github.com>
2020-11-26 14:07:53 -0500

    Update Check Windows Servers.ps1
    Updated ErrorCode to CCMErrorCode

--- Check Windows Servers.ps1
+++ Check Windows Servers.ps1
@@ -10,70 +10,70 @@ Foreach ($Server in $Servers){
     #Check connection to $Server with ICMP
     if (Test-Connection -ComputerName $Server -Quiet -Count 2)
     {
-        #If connection was sucessful, attempt to pull updates from the server
-        Try {
-        $Application = Invoke-Command -cn -$Server {Get-WmiObject -Namespace 'root\ccm\clientdsk' -Class CCM_SoftwareUpdate}
+            #If connection was sucessful, attempt to pull updates from the server
+            Try {
+            $Application = Invoke-Command -cn -$Server {Get-WmiObject -Namespace 'root\ccm\clientdsk' -Class CCM_SoftwareUpdate}
 
-        #If Application is empty, there are no CCM updates available at this time
-        if (!$Application){
-            $Updates += New-Object PSObject -Property ([ordered]@{
-            n = 'ServerName'
-            e = $Server
-            ArticleID = ' ERROR '
-            Publisher = ' ERROR '
-            Software = ' ERROR '
-            Description = ' ERROR '
-            State = ' ERROR '
-            ErrorCode = '0x123456789'})
-            }
-        else {
-            Foreach ($App in $Application) {
-                $EvState = Switch ($App.EvaluationState){
-                    '0' {}
-                    '1' {}
-                    '2' {}
-                    '3' {}
-                    '4' {}
-                    '5' {}
-                    '6' {}
-                    '7' {}
-                    '8' {}
-                    '9' {}
-                    '10' {}
-                    '11' {}
-                    '12' {}
-                    '13' {}
-                    '14' {}
-                    '15' {}
-                    '16' {}
-                    '17' {}
-                    '18' {}
-                    '19' {}
-                    '20' {}
-                    '21' {}
-                    '22' {}
-                    '23' {}
+            #If Application is empty, there are no CCM updates available at this time
+            if (!$Application){
+                $Updates += New-Object PSObject -Property ([ordered]@{
+                n = 'ServerName'
+                e = $Server
+                ArticleID = ' ERROR '
+                Publisher = ' ERROR '
+                Software = ' ERROR '
+                Description = ' ERROR '
+                State = ' ERROR '
+                CCMErrorCode = 'ERROR'})
+                }
+            else {
+                Foreach ($App in $Application) {
+                    $EvState = Switch ($App.EvaluationState){
+                        '0' {}
+                        '1' {}
+                        '2' {}
+                        '3' {}
+                        '4' {}
+                        '5' {}
+                        '6' {}
+                        '7' {}
+                        '8' {}
+                        '9' {}
+                        '10' {}
+                        '11' {}
+                        '12' {}
+                        '13' {}
+                        '14' {}
+                        '15' {}
+                        '16' {}
+                        '17' {}
+                        '18' {}
+                        '19' {}
+                        '20' {}
+                        '21' {}
+                        '22' {}
+                        '23' {}
+                    }
                 }
             }
         }
-    }
-    Catch {
-        Write-Host "Error pulling updates for $Server"
-        Write-Host $_.ErrorDetails.Message
+        Catch {
+            Write-Host "Error pulling updates for $Server"
+            Write-Host $_.ErrorDetails.Message
 
-        #Write failed server name on a new line with error message
-        $Updates += New-Object PSObject -Property ([ordered]@{
-        n = 'ServerName'
-        e = $Server
-        ArticleID = ' ERROR INVOKING COMMAND '
-        Publisher = ' ERROR INVOKING COMMAND '
-        Software = ' ERROR INVOKING COMMAND '
-        Description = ' ERROR INVOKING COMMAND '
-        State = ' ERROR INVOKING COMMAND '
-        ErrorCode = '0x123456789'})
-        Continue
+            #Write failed server name on a new line with error message
+            $Updates += New-Object PSObject -Property ([ordered]@{
+            n = 'ServerName'
+            e = $Server
+            ArticleID = ' ERROR INVOKING COMMAND '
+            Publisher = ' ERROR INVOKING COMMAND '
+            Software = ' ERROR INVOKING COMMAND '
+            Description = ' ERROR INVOKING COMMAND '
+            State = ' ERROR INVOKING COMMAND '
+            CCMErrorCode = 'ERROR INVOKING COMMAND'})
+            Continue
+            }
         }
-    }
     #If failed, create a line in the $Updates array that says it errored out
     else
     {
@@ -87,7 +87,7 @@ Foreach ($Server in $Servers){
         Software = ' CONNECTION ERROR '
         Description = ' CONNECTION ERROR '
         State = ' CONNECTION ERROR '
-        ErrorCode = '0x123456789'})
+        CCMErrorCode = 'ERROR INVOKING COMMAND'})
         Continue
     }
 }
@@ -99,6 +99,6 @@ Foreach ($Server in $Servers){
         Software = ' TEST '
         Description = ' TEST '
         State = ' TEST '
-        ErrorCode = '0x123456789'})
+        CCMErrorCode = '0x123456789'})
 
 $Updates | Out-GridView -Title "Updates"
