1925f756f139ce7541e705e49f3d516b9c7de97e
qmph22 <66791972+qmph22@users.noreply.github.com>
2020-11-26 13:57:24 -0500

    Update Check Windows Servers.ps1
    Added ErrorCode field for each update.

--- Check Windows Servers.ps1
+++ Check Windows Servers.ps1
@@ -1,4 +1,4 @@
-﻿#Define Servers
+#Define Servers
 $Servers = 'server1', 'server2', 'server3'
 
 #Define Array (empty)
@@ -7,47 +7,98 @@ $Updates = @()
 #Iterate attempt to connect to servers. Continue foreach loop if server cannot be reached.
 Foreach ($Server in $Servers){
 
-#Check connection to $Server with ICMP
-if (Test-Connection -ComputerName $Server -Quiet -Count 2)
-{
-#If connection was sucessful, attempt to pull updates from the server
-Try {
-$Application = Invoke-Command -cn -$Server {Get-WmiObject -Namespace 'root\ccm\clientdsk' -Class CCM_SoftwareUpdate}
+    #Check connection to $Server with ICMP
+    if (Test-Connection -ComputerName $Server -Quiet -Count 2)
+    {
+        #If connection was sucessful, attempt to pull updates from the server
+        Try {
+        $Application = Invoke-Command -cn -$Server {Get-WmiObject -Namespace 'root\ccm\clientdsk' -Class CCM_SoftwareUpdate}
 
-#If Application is empty, there are no CCM updates available at this time
+        #If Application is empty, there are no CCM updates available at this time
+        if (!$Application){
+            $Updates += New-Object PSObject -Property ([ordered]@{
+            n = 'ServerName'
+            e = $Server
+            ArticleID = ' ERROR '
+            Publisher = ' ERROR '
+            Software = ' ERROR '
+            Description = ' ERROR '
+            State = ' ERROR '
+            ErrorCode = '0x123456789'})
+            }
+        else {
+            Foreach ($App in $Application) {
+                $EvState = Switch ($App.EvaluationState){
+                    '0' {}
+                    '1' {}
+                    '2' {}
+                    '3' {}
+                    '4' {}
+                    '5' {}
+                    '6' {}
+                    '7' {}
+                    '8' {}
+                    '9' {}
+                    '10' {}
+                    '11' {}
+                    '12' {}
+                    '13' {}
+                    '14' {}
+                    '15' {}
+                    '16' {}
+                    '17' {}
+                    '18' {}
+                    '19' {}
+                    '20' {}
+                    '21' {}
+                    '22' {}
+                    '23' {}
+                }
+            }
+        }
+    }
+    Catch {
+        Write-Host "Error pulling updates for $Server"
+        Write-Host $_.ErrorDetails.Message
 
+        #Write failed server name on a new line with error message
+        $Updates += New-Object PSObject -Property ([ordered]@{
+        n = 'ServerName'
+        e = $Server
+        ArticleID = ' ERROR INVOKING COMMAND '
+        Publisher = ' ERROR INVOKING COMMAND '
+        Software = ' ERROR INVOKING COMMAND '
+        Description = ' ERROR INVOKING COMMAND '
+        State = ' ERROR INVOKING COMMAND '
+        ErrorCode = '0x123456789'})
+        Continue
+        }
+    }
+    #If failed, create a line in the $Updates array that says it errored out
+    else
+    {
+        Write-Host "Error connecting to $Server"
+        Write-Host $_.ErrorDetails.Message
+        $Updates += New-Object PSObject -Property ([ordered]@{
+        n = 'ServerName'
+        e = $Server
+        ArticleID = ' CONNECTION ERROR '
+        Publisher = ' CONNECTION ERROR '
+        Software = ' CONNECTION ERROR '
+        Description = ' CONNECTION ERROR '
+        State = ' CONNECTION ERROR '
+        ErrorCode = '0x123456789'})
+        Continue
+    }
 }
-Catch {
-Write-Host "Error pulling updates for $Server"
-Write-Host $_.ErrorDetails.Message
-
-#Write failed server name on a new line with error message
-$Updates += New-Object PSObject -Property ([ordered]@{
-n = 'ServerName'
-e = $Server
-ArticleID = ' ERROR '
-Publisher = ' ERROR '
-Software = ' ERROR '
-Description = ' ERROR '
-State = ' ERROR '})
-Continue
-}
-}
-#If failed, create a line in the $Updates array that says it errored out
-else
-{
-Write-Host "Error connecting to $Server"
-Write-Host $_.ErrorDetails.Message
-$Updates += New-Object PSObject -Property ([ordered]@{
-n = 'ServerName'
-e = $Server
-ArticleID = ' ERROR '
-Publisher = ' ERROR '
-Software = ' ERROR '
-Description = ' ERROR '
-State = ' ERROR '})
-Continue
-}
-}
+        $Updates += New-Object PSObject -Property ([ordered]@{
+        n = 'ServerName'
+        e = $Server
+        ArticleID = ' TEST '
+        Publisher = ' TEST '
+        Software = ' TEST '
+        Description = ' TEST '
+        State = ' TEST '
+        ErrorCode = '0x123456789'})
 
 $Updates | Out-GridView -Title "Updates"
