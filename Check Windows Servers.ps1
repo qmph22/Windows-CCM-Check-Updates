cfcf9746696e615872f8f9d6f2b044557dd7538d
qmph22 <66791972+qmph22@users.noreply.github.com>
2020-11-26 21:01:07 -0500

    Update Check Windows Servers.ps1
    Every server now runs as a separate job. Each job outputs an array that gets added to $Updates after the completion of every job. Added Read-Host command at the end of the script to support non-ISE environments

--- Check Windows Servers.ps1
+++ Check Windows Servers.ps1
@@ -1,32 +1,38 @@
 #Define Servers
-$Servers = 'server1', 'server2', 'server3'
+$Servers = 'server1', 'server2', 'server3','server1', 'server2', 'server3','server1', 'server2', 'server3'
 
 #Define Array (empty)
 $Updates = @()
 
-#Iterate attempt to connect to servers. Continue foreach loop if server cannot be reached.
-Foreach ($Server in $Servers){
+#Define Job List (empty)
+$Jobs = @()
 
-    #Check connection to $Server with ICMP
-    if (Test-Connection -ComputerName $Server -Quiet -Count 2)
+#Iterate attempt to connect to servers. Continue foreach loop if server cannot be reached.
+Write-Host "Checking servers: $Servers"
+Foreach ($Server in $Servers){
+#Begin seperate job for each server to reduce time spent running this entire script
+$Jobs += Start-Job -ScriptBlock {
+    #Check connection to $Using:Server with ICMP. $Using is to be able to send local variables to remote commands
+    if (Test-Connection -ComputerName $Using:Server -Quiet -Count 2)
     {
             #If connection was sucessful, attempt to pull updates from the server
             Try {
-            $Application = Invoke-Command -cn -$Server {Get-WmiObject -Namespace 'root\ccm\clientdsk' -Class CCM_SoftwareUpdate}
+            $Application = Invoke-Command -cn -$Using:Server {Get-WmiObject -Namespace 'root\ccm\clientdsk' -Class CCM_SoftwareUpdate}
 
             #If Application is empty, there are no CCM updates available at this time
             if (!$Application){
-                $Updates += New-Object PSObject -Property ([ordered]@{
+                New-Object PSObject -Property ([ordered]@{
                 n = 'ServerName'
-                e = $Server
-                ArticleID = ' ERROR '
-                Publisher = ' ERROR '
-                Software = ' ERROR '
-                Description = ' ERROR '
-                State = ' ERROR '
-                CCMErrorCode = 'ERROR'})
+                e = $Using:Server
+                ArticleID = ' - '
+                Publisher = ' - '
+                Software = ' - '
+                Description = ' - '
+                State = ' - '
+                CCMErrorCode = ' - '})
                 }
             else {
+                #Get evaluation states for each update found.
                 Foreach ($App in $Application) {
                     $EvState = Switch ($App.EvaluationState){
                         '0' {}
@@ -54,17 +60,23 @@ Foreach ($Server in $Servers){
                         '22' {}
                         '23' {}
                     }
+                New-Object PSObject -Property ([ordered]@{
+                n = 'ServerName'
+                e = $Using:Server
+                ArticleID = $App.ArticleID
+                Publisher = $App.Publisher
+                Software = $App.Name
+                Description = $App.Description
+                State = $EvState
+                CCMErrorCode = $ErrorCode})
                 }
             }
         }
         Catch {
-            Write-Host "Error pulling updates for $Server"
-            Write-Host $_.ErrorDetails.Message
-
             #Write failed server name on a new line with error message
-            $Updates += New-Object PSObject -Property ([ordered]@{
+            New-Object PSObject -Property ([ordered]@{
             n = 'ServerName'
-            e = $Server
+            e = $Using:Server
             ArticleID = ' ERROR INVOKING COMMAND '
             Publisher = ' ERROR INVOKING COMMAND '
             Software = ' ERROR INVOKING COMMAND '
@@ -77,28 +89,35 @@ Foreach ($Server in $Servers){
     #If failed, create a line in the $Updates array that says it errored out
     else
     {
-        Write-Host "Error connecting to $Server"
-        Write-Host $_.ErrorDetails.Message
-        $Updates += New-Object PSObject -Property ([ordered]@{
+        New-Object PSObject -Property ([ordered]@{
         n = 'ServerName'
-        e = $Server
+        e = $Using:Server
         ArticleID = ' CONNECTION ERROR '
         Publisher = ' CONNECTION ERROR '
         Software = ' CONNECTION ERROR '
         Description = ' CONNECTION ERROR '
         State = ' CONNECTION ERROR '
-        CCMErrorCode = 'ERROR INVOKING COMMAND'})
+        CCMErrorCode = 'CONNECTION ERROR'})
         Continue
+    }
     }
 }
         $Updates += New-Object PSObject -Property ([ordered]@{
         n = 'ServerName'
-        e = $Server
+        e = 'TestServer'
         ArticleID = ' TEST '
         Publisher = ' TEST '
         Software = ' TEST '
         Description = ' TEST '
         State = ' TEST '
-        CCMErrorCode = '0x123456789'})
+        CCMErrorCode = '0x12345678'})
 
+Write-Host 'Waiting for jobs to finish'
+#Wait for jobs to complete. Supress output
+Get-Job | Wait-Job | Out-Null
+
+#Add results of all jobs to $Updates and display
+$Updates += Receive-Job -Job $Jobs
 $Updates | Out-GridView -Title "Updates"
+
+Read-Host 'Script has ran sucessfully. Press Enter to exit'
